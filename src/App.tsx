c09d869bd7847cc0687ac050f591e13eda6af979
Israel Z <123hartk123@gmail.com>
2021-02-12 19:11:17 +0200

    adding changes to main app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import Background from './components/background';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <Background />
+    <div className="wheels-app">
+      <div className="header">
+          <div className="left-side">
+            <img src={"https://scontent.fsdv1-2.fna.fbcdn.net/v/t1.0-9/1655987_10152772697488469_7371064372680920844_n.jpg?_nc_cat=108&ccb=3&_nc_sid=09cbfe&_nc_ohc=oOKtUGuw_rkAX_DAEul&_nc_ht=scontent.fsdv1-2.fna&oh=071cfd895bba71403f138341d3149a6c&oe=604D81F5"}/>
+          </div>
+          <div className="right-side">
+            <div className="logo">גלגלים</div>
+            <div className="greeding">היי רן גורדון</div>
+          </div>
+      </div>
+      <div className="claims">
+        <div className="claim">תביעה חדשה</div>
+        <div className="claim">תביעה קיימת</div>
+      </div>
+      <div className="footer">
+      </div>
     </div>
+    </>
   );
 }
 
